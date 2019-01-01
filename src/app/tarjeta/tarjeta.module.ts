3da438798efabe31be00301d921b37e4ebdc74fa
guty <nggutierrez.45@gmail.com>
2019-01-01 17:07:07 -0300

    Arreglo en módulo tarjeta y sidenav de prueba

--- src/app/tarjeta/tarjeta.module.ts
+++ src/app/tarjeta/tarjeta.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TarjetaRoutingModule } from './tarjeta-routing.module';
+import { MaterialModule } from './../material/material.module';
+
 import { ListTarjetaComponent } from './list-tarjeta/list-tarjeta.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+const componentes = [
+  ListTarjetaComponent,
+]
 
 @NgModule({
-  declarations: [ListTarjetaComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [componentes],
   imports: [
     CommonModule,
-    TarjetaRoutingModule
-  ]
+    TarjetaRoutingModule,
+    MaterialModule
+  ],
+  exports: [componentes]
 })
 export class TarjetaModule { }
