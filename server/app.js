d5c88299a60d5305c98d5d7cbfed02aff10bea82
guty <nggutierrez.45@gmail.com>
2019-02-11 18:53:38 -0300

    Manejo de errores simple con morgan

--- server/app.js
+++ server/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const usuarioRoutes = require('./api/routes/usuarios');
 
+app.use(morgan('dev'));
+
 app.use('/usuarios', usuarioRoutes);
+
+app.use((req, res, cb) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    cb(error);
+});
+
+app.use((error, req, res, cb) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            mensaje: error.mensaje
+        }
+    });
+});
 
 module.exports = app;
