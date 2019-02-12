3f97762d83002dbef6ff3c710b89d9c3f96a7539
guty <nggutierrez.45@gmail.com>
2019-02-11 21:56:29 -0300

    Parsing the Body & Handling CORS

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const usuarioRoutes = require('./api/routes/usuarios');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, cb) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    cb();
+});
 
 app.use('/usuarios', usuarioRoutes);
 
